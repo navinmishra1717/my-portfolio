f2e29e60e582103ddab65e21b6460cdb65dd5eac
navin mishra <navinmishra1717@gmail.com>
2024-02-25 16:12:34 +0545

    next config changes

--- next.config.js
+++ next.config.js
@@ -1,11 +1,17 @@
-/** @type {import('next').NextConfig} */
+/**
+ * @type {import('next').NextConfig}
+ */
 const nextConfig = {
   output: "export",
-  experimental: {
-    serverActions: {
-      //   allowedOrigins: ["my-proxy.com", "*.my-proxy.com"],
-    },
-  },
+
+  // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
+  // trailingSlash: true,
+
+  // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
+  // skipTrailingSlashRedirect: true,
+
+  // Optional: Change the output directory `out` -> `dist`
+  // distDir: 'dist',
 };
 
-export default nextConfig;
+module.exports = nextConfig;
