1b4f2ebe91199baabe89d8427e792e20ecafb9a3
navin mishra <navinmishra1717@gmail.com>
2024-02-25 16:05:35 +0545

    next config changes

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export",
   experimental: {
     serverActions: {
       //   allowedOrigins: ["my-proxy.com", "*.my-proxy.com"],
