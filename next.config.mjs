5b337d67656833a1e01e8002cbd261cd4258e1ab
Navin Mishra <navinmishra1717@gmail.com>
2024-09-30 15:53:25 +0545

    export change

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,7 @@
 const nextConfig = {
   output: "export",
   basePath: "/my-portfolio",
+  distDir: "out",
   // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
   // trailingSlash: true,
   // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
